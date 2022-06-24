76fac762614e5c5cf4420ea89aa9c99afa1a36d4
AWu <rio888888@163.com>
2022-06-24 11:15:48 +0800

    whatsapp

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
 <template>
   <div id="home">
-    <img @click="openLink" style="width: 100%;" src="./assets/images/1.png" alt="">
+    <img style="width: 100%; max-width: 750px;" src="./assets/images/1.png" alt="">
     <div class="links">
       <ul>
-        <li>
-          <img src="./assets/images/Line.png" alt="">
+        <li @click="openLink('line')">
+          <img style="max-width: 80px; max-height: 80px;" src="./assets/images/Line.png" alt="">
         </li>
-        <li>
-          <img src="./assets/images/WhatsApp.png" alt="">
+        <li @click="openLink('whatsapp')">
+          <img style="max-width: 80px; max-height: 80px;" src="./assets/images/WhatsApp.png" alt="">
         </li>
       </ul>
     </div>
@@ -15,6 +15,23 @@
 </template>
 
 <script setup>
+
+function openLink(type) {
+  switch (type) {
+    case 'line':
+      console.log('line')
+      window.location.href = ''
+      break;
+
+    case 'whatsapp':
+      console.log('whatsapp')
+      window.location.href = 'https://api.whatsapp.com/send/?phone=66631271873&text&app_absent=0'
+      break;
+  
+    default:
+      break;
+  }
+}
 </script>
 
 <style lang="scss">
